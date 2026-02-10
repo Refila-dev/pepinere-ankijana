cafdfe4352d72bb96471fc66a1ec9a4349d45cb7
Refila-dev <hei.zoarisoa@gmail.com>
2026-02-10 11:32:47 +0300

    feat: add a logo MARA

--- app/components/ui/navbar.tsx
+++ app/components/ui/navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div className="flex justify-center">
       <div className="inline-flex h-20 px-8 bg-gray-200 gap-50 items-center justify-center rounded-lg fixed shadow-4xl">
         <div className='w-17 h-17 rounded-full bg-black/50 flex justify-center items-center'>
-          <h1 className='font-bold'>FI-A</h1>
+        <Image className='rounded-full' src={"/Maitso-Ravina-Ankijana.png"} alt="Logo" width={68} height={68} />
         </div>
         <ul className="flex gap-10 text-black font-bold">
           <li>Pejy Fandraisana</li>
--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,37 @@ export default function Home() {
       <main className="bg-url('/public/plantes-bg.jpeg') bg-cover bg-center h-screen flex items-center justify-center">
         <div className="bg-black/50 bg-opacity-75 p-10 shadow-lg w-400 h-190 rounded-b-full rounded-tl-full">
           <h1 className="text-4xl text-white font-bold text-center mt-10">
-            Tongasoa eto amin'ny tranokala Zana-kazo Ankijana.
+            Tongasoa eto amin'ny tranokala Maitso Ravina Ankijana(MARA).
           </h1>
-          <p className="text-white text-xl text-center mt-30 w-auto">Ity tranokala ity dia namboarina mba ho fampiroboroboana ny tontolon'ny fambolen-kazo eto amin'ny faritra Itasy. 
-            Maro ireo vokatra amidy eto amin'ny tranokala fa ny ankamaroan'izy ireo dia ireo hazo miroborobo ato amin'ny faritra Itasy, 
-            anisan'izany ny Tapie, ny Kininina, ny Kasia, ny Manga, sns...
+          <p className="text-white text-xl text-center mt-30 w-auto">
+            Ity tranokala ity dia namboarina mba ho fampiroboroboana ny
+            tontolon'ny fambolen-kazo eto amin'ny faritra Itasy. Maro ireo
+            vokatra amidy eto amin'ny tranokala fa ny ankamaroan'izy ireo dia
+            ireo hazo miroborobo ato amin'ny faritra Itasy, anisan'izany ny
+            Tapie, ny Kininina, ny Kasia, ny Manga, sns... Lorem ipsum dolor
+            sit, amet consectetur adipisicing elit. Voluptatum, voluptas facere!
+            Praesentium perferendis deleniti nihil necessitatibus, incidunt
+            doloribus consequuntur? Quis quas atque consectetur vel vitae
+            reprehenderit laborum unde nulla placeat.
           </p>
           <div className="mt-30 flex justify-center items-center">
-            <button className="bg-green-500 text-white w-70 h-16 rounded-4xl animate-bounce hover:bg-green-800">Te hahafantatra misymisy kokoa</button>
+            <button className="bg-green-500 text-white w-70 h-16 rounded-4xl animate-bounce hover:cursor-pointer hover:bg-green-800 flex flex-col items-center justify-center gap-2">
+              Te hahafantatra misymisy kokoa
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+                />
+              </svg>
+            </button>
           </div>
         </div>
       </main>
