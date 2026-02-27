76b17a15359da5b1f203ffa07def60917b041e8f
Refila-dev <hei.zoarisoa@gmail.com>
2026-02-27 23:40:56 +0300

    chore: add the amelioration of the about page

--- app/components/ui/about-reponsable.tsx
+++ app/components/ui/about-reponsable.tsx
@@ -13,15 +13,43 @@ const AboutResponsable  = () => {
         </p>
       </div>
       <div className="flex gap-10 justify-center items-center w-1/2">
-        <p className="text-white text-center text-lg">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est at vero
-          temporibus sint iusto officia explicabo! Distinctio assumenda eius
-          omnis cumque quae dolore accusamus aperiam sequi consequuntur sint?
-          Architecto, quam? Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Soluta atque reprehenderit natus vel, voluptatum quidem illum
-          repudiandae? Deserunt ratione, accusantium fugit atque iusto, possimus
-          error voluptatibus placeat voluptate, odio magni.
-        </p>
+        <div>
+          <p className="text-white text-center text-lg">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est at
+            vero temporibus sint iusto officia explicabo! Distinctio assumenda
+            eius omnis cumque quae dolore accusamus aperiam sequi consequuntur
+            sint? Architecto, quam? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Soluta atque reprehenderit natus vel, voluptatum
+            quidem illum repudiandae? Deserunt ratione, accusantium fugit atque
+            iusto, possimus error voluptatibus placeat voluptate, odio magni.
+          </p>
+          <div className="flex justify-center items-center gap-5 mt-5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 640 640"
+              fill="currentColor"
+              className="size-7 text-green-200"
+            >
+              <path d="M576 320C576 178.6 461.4 64 320 64C178.6 64 64 178.6 64 320C64 440 146.7 540.8 258.2 568.5L258.2 398.2L205.4 398.2L205.4 320L258.2 320L258.2 286.3C258.2 199.2 297.6 158.8 383.2 158.8C399.4 158.8 427.4 162 438.9 165.2L438.9 236C432.9 235.4 422.4 235 409.3 235C367.3 235 351.1 250.9 351.1 292.2L351.1 320L434.7 320L420.3 398.2L351 398.2L351 574.1C477.8 558.8 576 450.9 576 320z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 640 640"
+              fill="currentColor"
+              className="size-7 text-green-200"
+            >
+              <path d="M512 96L127.9 96C110.3 96 96 110.5 96 128.3L96 511.7C96 529.5 110.3 544 127.9 544L512 544C529.6 544 544 529.5 544 511.7L544 128.3C544 110.5 529.6 96 512 96zM231.4 480L165 480L165 266.2L231.5 266.2L231.5 480L231.4 480zM198.2 160C219.5 160 236.7 177.2 236.7 198.5C236.7 219.8 219.5 237 198.2 237C176.9 237 159.7 219.8 159.7 198.5C159.7 177.2 176.9 160 198.2 160zM480.3 480L413.9 480L413.9 376C413.9 351.2 413.4 319.3 379.4 319.3C344.8 319.3 339.5 346.3 339.5 374.2L339.5 480L273.1 480L273.1 266.2L336.8 266.2L336.8 295.4L337.7 295.4C346.6 278.6 368.3 260.9 400.6 260.9C467.8 260.9 480.3 305.2 480.3 362.8L480.3 480z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 640 640"
+              fill="currentColor"
+              className="size-7 text-green-200"
+            >
+              <path d="M125.4 128C91.5 128 64 155.5 64 189.4C64 190.3 64 191.1 64.1 192L64 192L64 448C64 483.3 92.7 512 128 512L512 512C547.3 512 576 483.3 576 448L576 192L575.9 192C575.9 191.1 576 190.3 576 189.4C576 155.5 548.5 128 514.6 128L125.4 128zM528 256.3L528 448C528 456.8 520.8 464 512 464L128 464C119.2 464 112 456.8 112 448L112 256.3L266.8 373.7C298.2 397.6 341.7 397.6 373.2 373.7L528 256.3zM112 189.4C112 182 118 176 125.4 176L514.6 176C522 176 528 182 528 189.4C528 193.6 526 197.6 522.7 200.1L344.2 335.5C329.9 346.3 310.1 346.3 295.8 335.5L117.3 200.1C114 197.6 112 193.6 112 189.4z" />
+            </svg>
+          </div>
+        </div>
         <Image
           src="/respo.jpeg"
           alt="About Responsable"
@@ -41,7 +69,7 @@ const AboutResponsable  = () => {
       </div>
       <div className="flex flex-col gap-10 w-full bg-black/50">
         <div className="flex gap-10 mx-5 my-5">
-          <div className="flex flex-col justify-center items-center bg-black hover:bg-black/60 rounded-lg border-2 border-gray-500">
+          <div className="flex flex-col justify-center items-center bg-black hover:bg-black/60 hover:rounded-tl-full rounded-lg border-2 border-gray-500">
             <Image
               src="/user.jpeg"
               alt="About Responsable"
@@ -81,7 +109,7 @@ const AboutResponsable  = () => {
               voluptatibus placeat voluptate, odio magni.
             </p>
           </div>
-          <div className="flex flex-col justify-center items-center bg-black hover:bg-black/60 rounded-lg border-2 border-gray-500">
+          <div className="flex flex-col justify-center items-center bg-black hover:bg-black/60 hover:rounded-tr-full rounded-lg border-2 border-gray-500">
             <Image
               src="/user.jpeg"
               alt="About Responsable"
@@ -122,7 +150,7 @@ const AboutResponsable  = () => {
               voluptatibus placeat voluptate, odio magni.
             </p>
           </div>
-          <div className="flex flex-col justify-center items-center bg-black hover:bg-black/60 rounded-lg border-2 border-gray-500">
+          <div className="flex flex-col justify-center items-center bg-black hover:bg-black/60 hover:rounded-tl-full rounded-lg border-2 border-gray-500">
             <Image
               src="/user.jpeg"
               alt="About Responsable"
@@ -164,7 +192,7 @@ const AboutResponsable  = () => {
           </div>
         </div>
         <div className="flex gap-10 mx-5 my-5">
-          <div className="flex flex-col justify-center items-center bg-black hover:bg-black/60 rounded-lg border-2 border-gray-500">
+          <div className="flex flex-col justify-center items-center bg-black hover:bg-black/60 hover:rounded-tr-full rounded-lg border-2 border-gray-500">
             <Image
               src="/user.jpeg"
               alt="About Responsable"
@@ -206,7 +234,7 @@ const AboutResponsable  = () => {
               voluptatibus placeat voluptate, odio magni.
             </p>
           </div>
-          <div className="flex flex-col justify-center items-center bg-black hover:bg-black/60 rounded-lg border-2 border-gray-500">
+          <div className="flex flex-col justify-center items-center bg-black hover:bg-black/60 hover:rounded-tl-full rounded-lg border-2 border-gray-500">
             <Image
               src="/user.jpeg"
               alt="about responsable"
@@ -247,7 +275,7 @@ const AboutResponsable  = () => {
               voluptatibus placeat voluptate, odio magni.
             </p>
           </div>
-          <div className="flex flex-col justify-center items-center bg-black hover:bg-black/60 rounded-lg border-2 border-gray-500">
+          <div className="flex flex-col justify-center items-center bg-black hover:bg-black/60 hover:rounded-tr-full rounded-lg border-2 border-gray-500">
             <Image
               src="/user.jpeg"
               alt="about responsable"
@@ -256,12 +284,12 @@ const AboutResponsable  = () => {
               className="rounded-full shadow-l-4xl px-4 py-4"
             />
             <h1 className="text-lg text-green-200">MBOLATIANA Eric.</h1>
-            <div className="flex">
+            <div className="flex gap-1">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 640 640"
                 fill="currentColor"
-                className="size-6 text-green-200"
+                className="size-6 text-green-200 hover:cursor-pointer"
               >
                 <path d="M576 320C576 178.6 461.4 64 320 64C178.6 64 64 178.6 64 320C64 440 146.7 540.8 258.2 568.5L258.2 398.2L205.4 398.2L205.4 320L258.2 320L258.2 286.3C258.2 199.2 297.6 158.8 383.2 158.8C399.4 158.8 427.4 162 438.9 165.2L438.9 236C432.9 235.4 422.4 235 409.3 235C367.3 235 351.1 250.9 351.1 292.2L351.1 320L434.7 320L420.3 398.2L351 398.2L351 574.1C477.8 558.8 576 450.9 576 320z" />
               </svg>
