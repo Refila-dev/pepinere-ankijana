bc869461262cf64dab47409e3f6a59baff355b67
Refila-dev <hei.zoarisoa@gmail.com>
2026-02-27 23:25:47 +0300

    feat: add the row collaborator

--- app/components/ui/navbar.tsx
+++ app/components/ui/navbar.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const Navbar = () => {
   return (
     <div className="flex justify-center">
-      <div className="inline-flex h-20 px-8 bg-gray-200 gap-50 items-center justify-center rounded-lg fixed shadow-4xl">
+      <div className="inline-flex h-20 px-8 bg-gray-200 gap-50 items-center justify-center rounded-lg fixed shadow-4xl mt-5">
         <div className="w-17 h-17 rounded-full bg-black/50 flex justify-center items-center">
           <Image
             className="rounded-full"
